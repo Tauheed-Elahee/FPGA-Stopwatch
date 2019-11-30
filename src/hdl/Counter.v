03db8b394a2e5c8b76ca5183d296f662050e1064
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2019-11-29 22:08:52 -0500

    Add threshold to counter.

--- src/hdl/Counter.v
+++ src/hdl/Counter.v
@@ -3,7 +3,8 @@ module Counter (
     rst,  // reset
     enable,
     numberIn,
-    numberOut
+    numberOut,
+    threshold
   );
   parameter BASE = 10;
   parameter NUMBER_OF_NYBLES = 1;
@@ -13,6 +14,7 @@ module Counter (
   input wire enable;
   input wire [(NUMBER_OF_NYBLES*4-1):0] numberIn;
   output reg [(NUMBER_OF_NYBLES*4-1):0] numberOut;
+  output reg threshold;
   
   /* Sequential Logic */
   // Async rst
@@ -21,6 +23,7 @@ module Counter (
       numberOut <= 8'b0;
     end else if (enable) begin
       numberOut <= ((0 <= numberIn) && (numberIn < (BASE-1)))? numberIn+1:0;  // numberIn must be between [0:BASE-1[ for numberOut to be [0:BASE[ and be of base BASE
+      threshold <= (numberOut == (BASE-2))? 1:0;
     end
   end
   
