5ddd85dc5b0904a5db76ed1f9b6c80f7b7edc751
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2019-12-11 18:09:57 -0500

    Fixed the reset debouncer to XNOR

--- src/hdl/reset_debouncer.v
+++ src/hdl/reset_debouncer.v
@@ -8,7 +8,7 @@ module reset_debouncer(
     reg [3:0] compare;
     
     // stable is a 4 bit xnor comparision
-    wire stable = (compare[0]^compare[1])^(compare[2]^compare[3]);
+    wire stable = (compare[0]~^compare[1])~^(compare[2]~^compare[3]);
     
     always@(posedge clk) begin
         compare[3] <= compare[2];
@@ -16,7 +16,7 @@ module reset_debouncer(
         compare[1] <= compare[0];
         compare[0] <= in;
         if (stable) begin
-            out <= ~compare[3];
+            out <= compare[3];
         end
     end
     
