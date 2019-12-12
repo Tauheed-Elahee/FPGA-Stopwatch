625a26fe69313ac6e3b37c9b9259915d90ce62b2
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2019-12-11 20:39:32 -0500

    Conter2 does not work anymore due to the black majic of Alchitry's 'understanding' of verilog.

--- src/hdl/Counter2.v
+++ src/hdl/Counter2.v
@@ -2,21 +2,23 @@ module Counter2 (
     clk,  // clock
     rst,  // reset
     enable,
+    up_down,
     numberIn,
     numberOut,
     threshold
   );
   parameter BASE = 10;
-  parameter NUMBER_OF_NYBLES = 1;
+  parameter NUMBER_OF_BITS = 4;
   
   input wire clk;
   input wire rst;
   input wire enable;
-  input wire [(NUMBER_OF_NYBLES*4-1):0] numberIn;
-  output wire [(NUMBER_OF_NYBLES*4-1):0] numberOut;
+  input wire up_down;
+  input wire [(NUMBER_OF_BITS-1):0] numberIn;
+  output wire [(NUMBER_OF_BITS-1):0] numberOut;
   output wire threshold;
   
-  Counter #(.BASE(BASE), .NUMBER_OF_NYBLES(NUMBER_OF_NYBLES)) counter(.clk(clk), .rst(rst), .enable(enable), .numberIn(numberIn), .numberOut(numberOut), .threshold(threshold));
+  Counter3 #(.BASE(BASE), .NUMBER_OF_BITS(NUMBER_OF_BITS)) counter(.clk(clk), .rst(rst), .enable(enable), .up_down(up_down), .numberIn(numberIn), .numberOut(numberOut), .threshold(threshold));
   
   
   
