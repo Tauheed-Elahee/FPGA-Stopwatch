01d098fb1d3d7fd2d9be8078ec7c5e2d87e7d0f0
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2019-12-21 16:24:01 -0500

    Attempted to change clock to use Counter. ALCHITRY DOES NOT UNDERSTAND VERILOGgit add --all!

--- src/hdl/Clock2.v
+++ src/hdl/Clock2.v
@@ -1,4 +1,4 @@
-module Clock (
+module Clock2 (
     input wire clk,  // clock
     input wire rst,
     output wire clkOut
@@ -12,33 +12,21 @@ module Clock (
 //  localparam OUTPUT_CLOCK_FREQUENCY_IN_HZ = 1/OUTPUT_CLOCK_PERIOD_IN_SECONDS;
 //   localparam MAX_COUNT = BOARD_CLOCK_FREQUENCY_IN_HZ / OUTPUT_CLOCK_FREQUENCY_IN_HZ;
 
-  localparam MAX_COUNT = BOARD_CLOCK_FREQUENCY_IN_HZ * OUTPUT_CLOCK_PERIOD_IN_SECONDS - 1;
+  localparam MAX_COUNT = BOARD_CLOCK_FREQUENCY_IN_HZ * OUTPUT_CLOCK_PERIOD_IN_SECONDS;
   
   
   // 10111 11010 11110 00010 00000 00 //27 bit
-  reg [63:0] count;
+  wire [63:0] count;
   
   assign clkOut = ~|count;
-  /*
-  Counter3 #(.BASE(MAX_COUNT), .NUMBER_OF_BITS(64)) counter( .clk(clk),
+  
+  Counter2 #(.BASE(MAX_COUNT), .NUMBER_OF_BITS(64)) counter( .clk(clk),
                                                             .rst(rst),
                                                             .enable(1),
                                                             .up_down(1),
                                                             .numberIn(count),
                                                             .numberOut(count)
                                                           );
-  */
-  /* Sequential Logic */
-  always @(posedge clk, posedge rst) begin
-    if (rst) begin
-      count <= MAX_COUNT;
-    end else begin
-      if (count <= MAX_COUNT) begin
-        count <= count - 1;
-      end else begin
-        count <= MAX_COUNT;
-      end
-    end
-  end
+  
   
 endmodule
