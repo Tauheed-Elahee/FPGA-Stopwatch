82c69be3d87f0c5e2e0de76d095607e94bfbfb9d
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2019-12-23 18:25:22 -0500

    Moved Counters to CounterModule. Changed refresh rate 120 Hz

---
+++ src/hdl/CounterModule.v
@@ -0,0 +1,77 @@
+module CounterModule (   
+    clk,  // clock
+    rst,  // reset
+    //enable, // needed but not implemented now.
+    up_down,
+    number
+  );
+  parameter NUMBER_OF_DIGITS = 4;
+  parameter NUMBER_OF_BITS_PER_DIGIT = 4;
+  
+  localparam NUMBER_OF_BITS = NUMBER_OF_DIGITS * NUMBER_OF_BITS_PER_DIGIT;
+  
+  input wire clk;
+  input wire rst;
+  //input wire enable;
+  input wire up_down;
+  input wire [(NUMBER_OF_BITS-1):0] number;
+  
+  wire clock_seconds_1;
+  wire [3:0] seconds_1 = number[3:0];
+  wire [3:0] seconds_10 = number[7:4];
+  wire [3:0] minutes_1 = number[11:8];
+  wire [3:0] minutes_10 = number[15:12];
+  
+  wire [6:0] threshold;
+  
+  // does not handle division in the inputs properly.
+    Clock #(  .BOARD_CLOCK_FREQUENCY_IN_HZ(100_000_000), // Alchitry does not understand Verilog and as such changing how numbers are update changes the display even though it shouldn't.
+              .OUTPUT_CLOCK_PERIOD_IN_SECONDS(1)
+           )
+           one_second_clock(  .clk(clk),
+                              .rst(rst),
+                              .clkOut(clock_seconds_1)
+                           );
+    
+    assign threshold[1] = clock_seconds_1 & threshold[0];
+    assign threshold[3] = threshold[1] & threshold[2];
+    assign threshold[5] = threshold[3] & threshold[4];
+    
+    // Alchitry labs cannot understand project hierachry and so a seperate Counter file must be created in order for Alchitry Labs to tell Vivado where it is.
+    // The doublicate counter was changed to a wrapper for the counter file.
+    
+    Counter2 #(.BASE(10), .NUMBER_OF_BITS(NUMBER_OF_BITS_PER_DIGIT)) counterSeconds1(  .clk(clk),
+                                            .rst(rst),
+                                            .enable(clock_seconds_1),
+                                            .up_down(up_down),
+                                            .numberIn(seconds_1[3:0]),
+                                            .numberOut(seconds_1[3:0]),
+                                            .threshold(threshold[0])
+                                         );
+    Counter2 #(.BASE(6), .NUMBER_OF_BITS(NUMBER_OF_BITS_PER_DIGIT)) counterSeconds10(  .clk(clk),
+                                            .rst(rst),
+                                            .enable(threshold[1]),
+                                            .up_down(up_down),
+                                            .numberIn(seconds_10[3:0]),
+                                            .numberOut(seconds_10[3:0]),
+                                            .threshold(threshold[2])
+                                         );
+    
+    Counter2 #(.BASE(10), .NUMBER_OF_BITS(NUMBER_OF_BITS_PER_DIGIT)) counterMinutes1(  .clk(clk),
+                                            .rst(rst),
+                                            .enable(threshold[3]),
+                                            .up_down(up_down),
+                                            .numberIn(minutes_1[3:0]),
+                                            .numberOut(minutes_1[3:0]),
+                                            .threshold(threshold[4])
+                                         );
+    Counter2 #(.BASE(6), .NUMBER_OF_BITS(NUMBER_OF_BITS_PER_DIGIT)) counterMinutes10(  .clk(clk),
+                                            .rst(rst),
+                                            .enable(threshold[5]),
+                                            .up_down(up_down),
+                                            .numberIn(minutes_10[3:0]),
+                                            .numberOut(minutes_10[3:0]),
+                                            .threshold(threshold[6])
+                                         );
+  
+endmodule
--- src/hdl/au_top.v
+++ src/hdl/au_top.v
@@ -11,95 +11,38 @@ module au_top(
     );
     
     wire rst;
-    wire clock_seconds_1;
-    wire [3:0] seconds_1;
-    wire [3:0] seconds_10;
-    wire [3:0] minutes_1;
-    wire [3:0] minutes_10;
-    
-    wire [6:0] threshold;
     
     reset_debouncer reset_conditioner(  .clk(clk),
                                         .in(!rst_n),
                                         .out(rst)
                                      );
     
-    
-    // does not handle division in the inputs properly.
-    Clock #(  .BOARD_CLOCK_FREQUENCY_IN_HZ(100_000_000), // Alchitry does not understand Verilog and as such changing how numbers are update changes the display even though it shouldn't.
-              .OUTPUT_CLOCK_PERIOD_IN_SECONDS(1)
-           )
-           one_second_clock(  .clk(clk),
-                              .rst(rst),
-                              .clkOut(clock_seconds_1)
-                           );
-    
-    assign threshold[1] = clock_seconds_1 & threshold[0];
-    assign threshold[3] = threshold[1] & threshold[2];
-    assign threshold[5] = threshold[3] & threshold[4];
-    
-    // Alchitry labs cannot understand project hierachry and so a seperate Counter file must be created in order for Alchitry Labs to tell Vivado where it is.
-    // The doublicate counter was changed to a wrapper for the counter file.
-    
-    Counter2 #(.BASE(10)) counterSeconds1(  .clk(clk),
-                                            .rst(rst),
-                                            .enable(clock_seconds_1),
-                                            .up_down(io_dip[0]),
-                                            .numberIn(seconds_1[3:0]),
-                                            .numberOut(seconds_1[3:0]),
-                                            .threshold(threshold[0])
-                                         );
-    Counter2 #(.BASE(6)) counterSeconds10(  .clk(clk),
-                                            .rst(rst),
-                                            .enable(threshold[1]),
-                                            .up_down(io_dip[0]),
-                                            .numberIn(seconds_10[3:0]),
-                                            .numberOut(seconds_10[3:0]),
-                                            .threshold(threshold[2])
-                                         );
-    
-    Counter2 #(.BASE(10)) counterMinutes1(  .clk(clk),
-                                            .rst(rst),
-                                            .enable(threshold[3]),
-                                            .up_down(io_dip[0]),
-                                            .numberIn(minutes_1[3:0]),
-                                            .numberOut(minutes_1[3:0]),
-                                            .threshold(threshold[4])
-                                         );
-    Counter2 #(.BASE(6)) counterMinutes10(  .clk(clk),
-                                            .rst(rst),
-                                            .enable(threshold[5]),
-                                            .up_down(io_dip[0]),
-                                            .numberIn(minutes_10[3:0]),
-                                            .numberOut(minutes_10[3:0]),
-                                            .threshold(threshold[6])
-                                         );
+    wire [15:0] number;
     
     
-    assign led [7:4] = seconds_10 [3:0];
-    assign led [3:0] = seconds_1 [3:0];
+    CounterModule #(.NUMBER_OF_DIGITS(4), .NUMBER_OF_BITS_PER_DIGIT(4)) counterModule(.clk(clk), .rst(rst), .up_down(io_dip[0]), .number(number[15:0]));
+    
+    
+    assign led [7:4] = number [7:4];
+    assign led [3:0] = number [3:0];
     
     assign io_led [23] = io_dip[0];
-    assign io_led [22] = |io_dip[23:1];
+    assign io_led [22] = io_dip[1];
+    assign io_led [21] = |io_dip[23:2];
     
-    assign io_led [21:16] = 16'b1111_1111;
-    assign io_led [15:12] = minutes_10 [3:0];
-    assign io_led [11:8] = minutes_1 [3:0];
-    assign io_led [7:4] = seconds_10 [3:0];
-    assign io_led [3:0] = seconds_1 [3:0];
+    assign io_led [20:16] = 16'b1111_1111;
+    assign io_led [15:12] = number [15:12];
+    assign io_led [11:8] = number [11:8];
+    assign io_led [7:4] = number [7:4];
+    assign io_led [3:0] = number [3:0];
     
     // It works with 3 displays but not 4. It is the combined dark majic of Alchitry and Vivado that prevents good verilog from being implemented. IT IS VERY PICKY ABOUT REFRESH RATES
     Display_Digits #(   .NUMBER_OF_DIGITS(4),
-                        .REFRESH_RATE_IN_HERTZ(400),
+                        .REFRESH_RATE_IN_HERTZ(120),
                         .BOARD_CLOCK_FREQUENCY_IN_HZ(100_000_000)
                     )
                     display_digits( .clk(clk),
-                                    .number( {  minutes_10[3:0],
-                                                minutes_1[3:0],
-                                                seconds_10[3:0],
-                                                seconds_1[3:0]
-                                              }
-                                           ),
+                                    .number(number[15:0]),
                                     .io_sel(io_sel),
                                     .io_seg(io_seg)
                     );
