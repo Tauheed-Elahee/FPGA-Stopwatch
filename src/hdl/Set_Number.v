320eb8a97f1c2f4b14bec2e0ccc77c418625caa9
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2019-12-24 19:14:41 -0500

    Add Set_Numbers module.

---
+++ src/hdl/Set_Number.v
@@ -0,0 +1,21 @@
+module Set_Number (
+    input clk,  // clock
+    input rst,  // reset
+    output out
+  );
+
+  /* Combinational Logic */
+  always @* begin
+    out = 0;
+  end
+  
+  /* Sequential Logic */
+  always @(posedge clk) begin
+    if (rst) begin
+      // Add flip-flop reset values here
+    end else begin
+      // Add flip-flop q <= d statements here
+    end
+  end
+  
+endmodule
