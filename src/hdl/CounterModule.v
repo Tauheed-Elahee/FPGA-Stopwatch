193ff4426cb722d55420a008b7c99ae81fb8565c
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2019-12-31 16:41:31 -0500

    Changed threshold wires to seperate enable and threshold wires in CounterModule.

--- src/hdl/CounterModule.v
+++ src/hdl/CounterModule.v
@@ -22,7 +22,8 @@ module CounterModule (
   wire [3:0] minutes_1 = number[11:8];
   wire [3:0] minutes_10 = number[15:12];
   
-  wire [6:0] threshold;
+  wire [(NUMBER_OF_DIGITS-1):0] enable_wire;
+  wire [(NUMBER_OF_DIGITS-1):0] threshold;
   
   // does not handle division in the inputs properly.
     Clock #(  .BOARD_CLOCK_FREQUENCY_IN_HZ(100_000_000), // Alchitry does not understand Verilog and as such changing how numbers are update changes the display even though it shouldn't.
@@ -33,16 +34,17 @@ module CounterModule (
                               .clkOut(clock_seconds_1)
                            );
     
-    assign threshold[1] = clock_seconds_1 & threshold[0];
-    assign threshold[3] = threshold[1] & threshold[2];
-    assign threshold[5] = threshold[3] & threshold[4];
+    assign enable_wire[0] = clock_seconds_1 & enable;
+    assign enable_wire[1] = enable_wire[0] & threshold[0];
+    assign enable_wire[2] = enable_wire[1] & threshold[1];
+    assign enable_wire[3] = enable_wire[2] & threshold[2];
     
     // Alchitry labs cannot understand project hierachry and so a seperate Counter file must be created in order for Alchitry Labs to tell Vivado where it is.
     // The doublicate counter was changed to a wrapper for the counter file.
     
     Counter #(.BASE(10), .NUMBER_OF_BITS(NUMBER_OF_BITS_PER_DIGIT)) counterSeconds1(  .clk(clk),
                                             .rst(rst),
-                                            .enable(clock_seconds_1&enable),
+                                            .enable(enable_wire[0]),
                                             .up_down(up_down),
                                             .numberIn(seconds_1[3:0]),
                                             .numberOut(seconds_1[3:0]),
@@ -50,28 +52,28 @@ module CounterModule (
                                          );
     Counter #(.BASE(6), .NUMBER_OF_BITS(NUMBER_OF_BITS_PER_DIGIT)) counterSeconds10(  .clk(clk),
                                             .rst(rst),
-                                            .enable(threshold[1]),
+                                            .enable(enable_wire[1]),
                                             .up_down(up_down),
                                             .numberIn(seconds_10[3:0]),
                                             .numberOut(seconds_10[3:0]),
-                                            .threshold(threshold[2])
+                                            .threshold(threshold[1])
                                          );
     
     Counter #(.BASE(10), .NUMBER_OF_BITS(NUMBER_OF_BITS_PER_DIGIT)) counterMinutes1(  .clk(clk),
                                             .rst(rst),
-                                            .enable(threshold[3]),
+                                            .enable(enable_wire[2]),
                                             .up_down(up_down),
                                             .numberIn(minutes_1[3:0]),
                                             .numberOut(minutes_1[3:0]),
-                                            .threshold(threshold[4])
+                                            .threshold(threshold[2])
                                          );
     Counter #(.BASE(6), .NUMBER_OF_BITS(NUMBER_OF_BITS_PER_DIGIT)) counterMinutes10(  .clk(clk),
                                             .rst(rst),
-                                            .enable(threshold[5]),
+                                            .enable(enable_wire[3]),
                                             .up_down(up_down),
                                             .numberIn(minutes_10[3:0]),
                                             .numberOut(minutes_10[3:0]),
-                                            .threshold(threshold[6])
+                                            .threshold(threshold[3])
                                          );
   
 endmodule
