1acb92afae8d02db336008ad164162212aaa4d9b
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2020-01-02 23:42:56 -0500

    Make number to switch between numberIn and numerOut based on EXPOSE_NUMBER parameter. It does not work on instatiation because Verilog.

--- src/hdl/Counter.v
+++ src/hdl/Counter.v
@@ -26,8 +26,9 @@ module Counter (
   wire [(NUMBER_OF_BITS-1):0] numberDecrement;
   
   /* Combinational Logic */
-  assign numberIncrement = ((0 <= numberIn) && (numberIn < (BASE-1)))? numberIn+1:0;  // numberIn must be between [0:BASE-1[ for numberOut to be [0:BASE[ and be of base BASE
-  assign numberDecrement = ((0 < numberIn) && (numberIn <= (BASE-1)))? numberIn-1:BASE-1;
+  assign number = (EXPOSE_NUMBER)? numberIn:numberOut;
+  assign numberIncrement = ((0 <= number) && (number < (BASE-1)))? number+1:0;  // numberIn must be between [0:BASE-1[ for numberOut to be [0:BASE[ and be of base BASE
+  assign numberDecrement = ((0 < number) && (number <= (BASE-1)))? number-1:BASE-1;
   assign numberNext =  (up_down)? numberIncrement:numberDecrement;
   assign threshold = (up_down)? ((numberOut == BASE-1)? 1:0):((numberOut == 0)? 1:0);
   
