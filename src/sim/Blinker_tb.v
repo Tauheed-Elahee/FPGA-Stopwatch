f1a723dea4c7a0cd6bdc5fc85837aad720783c34
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2020-01-18 00:22:34 -0500

    Complete enough of blinker_tb.v to test.

--- src/sim/Blinker_tb.v
+++ src/sim/Blinker_tb.v
@@ -1,4 +1,6 @@
 `timescale 10ns/1ns
+`include "../hdl/Counter.v"
+`include "../hdl/Clock.v"
 `include "../hdl/Blinker.v"
 
 /* commands to enter after running iverilog
@@ -19,7 +21,7 @@ module Blinker_tb();
     reg rst;
     wire blink;
     
-    Blinker #(.BOARD_CLOCK_FREQUENCY_IN_HZ(10)) blinker_tb(.clk(clk), .rst(rst), .blink(blink));
+    Blinker #(.BOARD_CLOCK_FREQUENCY_IN_HZ(100_000_000)) blinker_tb(.clk(clk), .rst(rst), .blink(blink));
     
     integer i = 0;
     
@@ -39,8 +41,9 @@ module Blinker_tb();
         rst = 0;
         #1;
         
-        for (i=0; i<100; i=i+1) begin
+        for (i=0; i<1000_000_000; i=i+1) begin
             clk <= !clk;
+            #1;
         end
         
     end
