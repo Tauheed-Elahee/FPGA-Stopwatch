dac7ff711eb21d5e46a20cfdc1b19a61b507dd5f
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2020-02-23 13:44:04 -0500

    Rearange all output assignments to the end.

--- src/hdl/au_top.v
+++ src/hdl/au_top.v
@@ -24,9 +24,6 @@ module au_top(
     CounterModule #(.NUMBER_OF_DIGITS(4), .NUMBER_OF_BITS_PER_DIGIT(4)) counterModule(.clk(clk), .rst(rst), .enable(io_dip[1]), .up_down(io_dip[0]), .number(number[15:0]));
     
     
-    assign led [7:4] = number [7:4];
-    assign led [3:0] = number [3:0];
-    
     assign io_led [23] = io_dip[0];
     assign io_led [22] = io_dip[1];
     assign io_led [21] = io_button[0];
@@ -37,6 +34,9 @@ module au_top(
     //assign io_led [16] = |io_dip[23:2];
     Blinker #(.BOARD_CLOCK_FREQUENCY_IN_HZ(100_000_000), .OUTPUT_CLOCK_FREQUENCY_IN_HZ(10)) blinker(.clk(clk), .rst(rst), .blink(io_led[16]));
     
+    
+    assign led [7:4] = number [7:4];
+    assign led [3:0] = number [3:0];
     
     assign io_led [15:12] = number [15:12];
     assign io_led [11:8] = number [11:8];
