ed4b2c93d808ebf2e5a6149cc7b7ebe252cd2640
Tauheed-Elahee <hobbies@tauheed-elahee.com>
2020-02-23 13:59:00 -0500

    Fix blinker frequency

--- src/sim/Blinker_tb.v
+++ src/sim/Blinker_tb.v
@@ -1,6 +1,6 @@
 `timescale 10ns/1ns
-`include "../hdl/Counter.v"
-`include "../hdl/Clock.v"
+`include "../hdl/Counter2.v"
+`include "../hdl/Clock_Hz.v"
 `include "../hdl/Blinker.v"
 
 /* commands to enter after running iverilog
@@ -21,7 +21,7 @@ module Blinker_tb();
     reg rst;
     wire blink;
     
-    Blinker #(.BOARD_CLOCK_FREQUENCY_IN_HZ(100_000_000)) blinker_tb(.clk(clk), .rst(rst), .blink(blink));
+    Blinker #(.BOARD_CLOCK_FREQUENCY_IN_HZ(20), .OUTPUT_CLOCK_FREQUENCY_IN_HZ(1)) blinker_tb(.clk(clk), .rst(rst), .blink(blink));
     
     integer i = 0;
     
@@ -41,7 +41,7 @@ module Blinker_tb();
         rst = 0;
         #1;
         
-        for (i=0; i<1000_000_000; i=i+1) begin
+        for (i=0; i<100; i=i+1) begin
             clk <= !clk;
             #1;
         end
